c503d61515385ea0bf3731d3434ee0f52a7a5360
biehl <biehlarthurv@gmail.com>
2025-04-16 19:45:32 -0300

    carrocel and cover

--- components/site/about.vue
+++ components/site/about.vue
@@ -1,5 +1,18 @@
 <template>
     <div class="w-full py-5 flex justify-center items-center bg-white">
+      <div class="w-full max-w-[1300px] flex flex-col items-center justify-center sm:items-center sm:justify-center ">
+        <div class="sm:w-[600px] text-center sm:h-[150px] flex flex-col">
+          <h2 class="sm:text-[30px] text-[18px] font-semibold text-[#434343] mt-2.5">Ao lado dos melhores bancos</h2>
+          <p class="sm:text-[15px] text-[12px] font-normal text-[#8a8a8a] break-words">Trabalhamos lado a lado com alguns dos líderes em cada setor para oferecer um portfólio completo e personalizado de produtos e serviços financeiros, que atendam às necessidades específicas de cada cliente.</p>
+        </div>
+        <SiteCarrocel />
+      </div>
+    </div>
+
+
+
+    <!-- topicos -->
+    <div class="w-full py-5 flex justify-center items-center bg-white"> 
       <div class="w-full max-w-[1300px] flex flex-wrap gap-3 items-center justify-center sm:items-center sm:justify-start">
         <div v-for="item in items" :key="item" class="sm:w-[400px] w-full sm:h-[250px] bg-white sm:border sm:border-[#e3e3e3] sm:rounded-md sm:shadow-md flex justify-center items-center transition-all duration-300">
           <div class="sm:w-[350px] w-[95%] sm:h-[200px] flex flex-col justify-start items-start overflow-hidden p-2 ">
@@ -47,5 +60,5 @@
   ];
   </script>
   
-  <style scoped></style>
+
   
